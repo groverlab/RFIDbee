d86dfbaf07909c7e3a61fd675934524258606e76
William H. Grover <wgrover@gmail.com>
2021-04-09 17:07:50 -0700

    initial steps toward clock-setting code

--- arduino/setup.py
+++ arduino/setup.py
@@ -1,4 +1,5 @@
-import os, sys
+import os, sys, serial
+from datetime import datetime
 
 port = ""
 usb_count = 0
@@ -13,9 +14,16 @@ if usb_count > 1:
     sys.exit("Multiple ports found")
 
 print(port)
-stream = os.popen("arduino-cli compile --fqbn arduino:avr:nano RFIDbee2")
-print(stream.read())
-stream = os.popen("arduino-cli upload -p /dev/" + port + " --fqbn arduino:avr:nano RFIDbee2")
-print(stream.read())
+# stream = os.popen("arduino-cli compile --fqbn arduino:avr:nano RFIDbee2")
+# print(stream.read())
+# stream = os.popen("arduino-cli upload -p /dev/" + port + " --fqbn arduino:avr:nano RFIDbee2")
+# print(stream.read())
+
+ser = serial.Serial("/dev/" + port, timeout=3)
+s = ser.read(10000)
+print(s)
+
+now = datetime.now()
+print(now.strftime("%y %m %d %H %M %S"))
 
 print("DONE")
