044ba5962e5d94ae25f7ed83a26d81d360140caf
William H. Grover <wgrover@gmail.com>
2021-04-29 15:26:31 -0700

    Create bootstrap.py

---
+++ arduino/bootstrap.py
@@ -0,0 +1,38 @@
+# bootstrap.py by William H. Grover
+# Automates setup of a new RFIDbee2 reader on a Mac
+
+import os, sys, serial, time
+
+port = ""
+usb_count = 0
+devices = os.listdir("/dev")
+for device in devices:
+    if "cu.usbserial" in device:
+        port = device
+        usb_count += 1
+if usb_count == 0:
+    sys.exit("No port found")
+if usb_count > 1:
+    sys.exit("Multiple ports found")
+
+port = "/dev/" + port
+
+# print(port)
+# stream = os.popen("arduino-cli compile --fqbn arduino:avr:nano RFIDbee2")
+# print(stream.read())
+stream = os.popen("arduino-cli upload -p " + port + " --fqbn arduino:avr:nano RFIDbee2")
+stream.read()
+# print("DONE UPLOADING")
+
+
+ser = serial.Serial(port, timeout=3)
+s = ser.read(10000)
+if not b"Startup\tStartup" in s:
+    sys.exit("No successful Arduino startup detected")
+
+timestring = time.strftime("%y %m %d %H %M %S", time.gmtime())
+# print(timestring)
+ser.write(bytes(timestring, 'utf-8'))
+
+print("DONE")
+
