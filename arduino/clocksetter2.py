9b5e42a51cd804de56af86c2b137f25002a5c94c
William H. Grover <wgrover@gmail.com>
2024-01-18 11:13:52 -0800

    automatically finds USB port in Windows

--- arduino/clocksetter2.py
+++ arduino/clocksetter2.py
@@ -7,9 +7,10 @@ import os, sys, serial, time
 from serial.tools.list_ports import comports
 
 
-port = ""
 
-# MAC:::
+
+# ORIGINAL MAC STUFF:
+# port = ""
 # usb_count = 0
 # devices = os.listdir("/dev")
 # for device in devices:
@@ -22,17 +23,15 @@ port = ""
 #     sys.exit("Multiple ports found")
 # port = "/dev/" + port
 
+port = False
 for p in comports():
     if "USB" in str(p):
-        print(str(p))
-exit()
-port = "COM3"
+        port = p.name
+
+if not port:
+    sys.exit("No port found with USB in the name")
 
 print(port)
-# stream = os.popen("arduino-cli compile --fqbn arduino:avr:nano RFIDbee2")
-# print(stream.read())
-# stream = os.popen("arduino-cli upload -p " + port + " --fqbn arduino:avr:nano RFIDbee2")
-# print(stream.read())
 
 ser = serial.Serial(port, timeout=3)
 s = ser.read(10000)
