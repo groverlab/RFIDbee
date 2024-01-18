ec27cb342595a9d010b518c1754c61997e19d689
William H. Grover <wgrover@gmail.com>
2024-01-18 13:41:25 -0800

    added some nice debug info

--- arduino/clocksetter2.py
+++ arduino/clocksetter2.py
@@ -7,23 +7,29 @@
 import sys, serial, time, platform
 from serial.tools.list_ports import comports
 
-port = False
+ports = 0
+port = ""
 for p in comports():
     if "USB" in str(p):
         port = p.name
+        ports += 1
 
-if not port:
-    sys.exit("No port found with USB in the name")
+if ports == 0:
+    sys.exit("No port found - is the RFIDbee2 plugged in?")
+
+if ports >= 2:
+    sys.exit("Too many ports found - unplug everything but the RFIDbee2 and try again")
 
 if platform.system() == "Darwin":  # if MacOS...
     port = "/dev/" + port   # ...prepend /dev/
 
-print(port)
+print("Found an RFIDbee2 at " + port)
 
 ser = serial.Serial(port, timeout=3)
 s = ser.read(10000)
-print(s)
+print(s.decode("utf-8"))
 
+print("Setting the clock using this time:   ", end="")
 timestring = time.strftime("%y %m %d %H %M %S", time.gmtime())
 print(timestring)
 ser.write(bytes(timestring, 'utf-8'))
